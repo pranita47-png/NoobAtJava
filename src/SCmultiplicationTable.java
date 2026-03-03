5d78f45493e63883ace0b9baadf68775dad3a3eb
Pranita <pranabhardwaj01@gmail.com>
2026-03-03 06:33:35 +0530

    Learning

--- src/SCmultiplicationTable.java
+++ src/SCmultiplicationTable.java
@@ -3,12 +3,12 @@ public class SCmultiplicationTable {
     public static void main(String[] arr){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the number whoses multiple you want- ");
-        int x,i,n;
+        int x,i,j;
         x=sc.nextInt();
-        System.out.println("Enter the number- ");
-        for(i=1; i<=10; i++){
-            n=i*x;
-            System.out.println(x+ " x " +i + " = " +n);
+        System.out.println("Enter the number till which you want the multiple- ");
+        j=sc.nextInt();
+        for(i=1; i<=j; i++){
+            System.out.println(x+ " x " +i + " = " +(x*i));
         }
 
     }
