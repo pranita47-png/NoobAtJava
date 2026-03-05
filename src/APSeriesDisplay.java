068389f7870fca8b52474d1bb91fd6a22907ae3b
Pranita <pranabhardwaj01@gmail.com>
2026-03-05 18:29:17 +0530

    Learning

--- src/APSeriesDisplay.java
+++ src/APSeriesDisplay.java
@@ -3,6 +3,7 @@ public class APSeriesDisplay {
     public static void main(String[] arr){
         Scanner sc=new Scanner(System.in);
         int a,d,n;
+        System.out.println("=====AP Series=====");
         System.out.println("Enter the value of first number i.e. 'a' : ");
         a=sc.nextInt();
         System.out.println("Enter the value of difference of number i.e. 'd' : ");
