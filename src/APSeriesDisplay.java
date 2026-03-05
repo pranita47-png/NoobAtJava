6ee0b2912d02a8021d93fcf28f7c2e9a23e24c26
Pranita <pranabhardwaj01@gmail.com>
2026-03-05 18:28:29 +0530

    Learning

---
+++ src/APSeriesDisplay.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class APSeriesDisplay {
+    public static void main(String[] arr){
+        Scanner sc=new Scanner(System.in);
+        int a,d,n;
+        System.out.println("Enter the value of first number i.e. 'a' : ");
+        a=sc.nextInt();
+        System.out.println("Enter the value of difference of number i.e. 'd' : ");
+        d=sc.nextInt();
+        System.out.println("Enter the numbers till you want to get value i.e. 'n' : ");
+        n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            int an=a+(i)*d;
+            System.out.printf("%d ",an);
+            //a=a+d;//System.out.printf(" %d ",a);//i++;
+        }
+    }
+}
