52291bf97cc93418ca835d5478cf2839d92d0d95
Pranita <pranabhardwaj01@gmail.com>
2026-04-15 01:21:12 +0530

    Learning

--- src/Practice1stTimer/CountDigits.java
+++ src/Practice1stTimer/CountDigits.java
@@ -3,22 +3,33 @@ package Practice1stTimer;
 import java.util.Scanner;
 
 public class CountDigits {
-    public static void main(String[] arr){
-        Scanner sc=new Scanner(System.in);
+    public static void main(String[] arr) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter- ");
-        int num=sc.nextInt();
-        int rev=0;
-        while(num>0){
-            int c=num%10;
-            rev=rev*10+c;
-            num=num/10;
-           // System.out.print(c);
+        int orgNum = sc.nextInt();
+        int num = Math.abs(orgNum);
+        if (num == 0) {
+            System.out.println(0);
         }
-        System.out.println();
-        while(rev>0){
-            int ch=rev%10;
-            rev=rev/10;
-            System.out.println(ch);
+        else {
+            if (orgNum < 0) {
+                System.out.println("-");
+            }
+
+            int num1 = num;
+            int count = 0;
+            while (num1 > 0) {
+                num1 = num1 / 10;
+                count++;
+            }
+            int div = (int) Math.pow(10, count - 1);
+            int num2;
+            while (div > 0) {
+                num2 = num / div;
+                num = num % div;
+                div = div / 10;
+                System.out.println(num2);
+            }
         }
     }
 }
